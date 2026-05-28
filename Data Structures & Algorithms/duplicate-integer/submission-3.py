411100bb041a01adde968fd32d7c05e40904de96
KulvanshJaswal <kulvansh.jaswal@ucalgary.ca>
2026-05-28 17:37:44 -0600

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        numbers = {}
+        for num in nums:
+            if num in numbers:
+                return True
+            numbers[num] = num
+        return False
+
+        
